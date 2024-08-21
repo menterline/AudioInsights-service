583ababf67a6576f25cfa4c4e4736a88ed25d476
Matt Enterline <matthew.j.enterline@gmail.com>
2024-08-20 21:50:39 -0400

    removed http params

--- src/main/kotlin/com/audio_insights_service/services/ISpotifyService.kt
+++ src/main/kotlin/com/audio_insights_service/services/ISpotifyService.kt
@@ -1,17 +1,15 @@
 package com.audio_insights_service.services
 
 import com.audio_insights_service.entities.*
-import org.springframework.web.bind.annotation.RequestHeader
-import org.springframework.web.bind.annotation.RequestParam
 
 interface ISpotifyService {
 
-    suspend fun fetchProfile(@RequestHeader("Authorization") bearerToken: String): UserProfile;
+    suspend fun fetchProfile( bearerToken: String): UserProfile
 
-    suspend fun fetchTopItems(bearerToken: String, @RequestParam term: String): TopItems
+    suspend fun fetchTopItems(bearerToken: String, term: String): TopItems
 
-    suspend fun fetchTopTracks(bearerToken: String, term: String): TopItemsResponse<Track>;
+    suspend fun fetchTopTracks(bearerToken: String, term: String): TopItemsResponse<Track>
 
-    suspend fun fetchTopArtists(bearerToken: String, term: String): TopItemsResponse<Artist>;
+    suspend fun fetchTopArtists(bearerToken: String, term: String): TopItemsResponse<Artist>
 
 }
