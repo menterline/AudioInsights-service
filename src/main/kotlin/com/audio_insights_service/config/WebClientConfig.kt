0990fe7c9638e1cac2d23341bca431e4152484a7
Matt Enterline <matthew.j.enterline@gmail.com>
2024-08-20 22:16:48 -0400

    Moved all fetches to repository and added repository tests

--- src/main/kotlin/com/audio_insights_service/config/WebClientConfig.kt
+++ src/main/kotlin/com/audio_insights_service/config/WebClientConfig.kt
@@ -1,5 +1,9 @@
 package com.audio_insights_service.config
 
+import com.audio_insights_service.repositories.ISpotifyRepository
+import com.audio_insights_service.repositories.SpotifyRepository
+import com.audio_insights_service.services.ISpotifyService
+import com.audio_insights_service.services.SpotifyService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.client.WebClient
@@ -10,5 +14,14 @@ class WebClientConfig {
     fun spotifyWebClient(): WebClient {
         return WebClient.builder().baseUrl("https://api.spotify.com")
             .build()
+    }
+    @Bean
+    fun spotifyRepository(): ISpotifyRepository {
+        return SpotifyRepository(spotifyWebClient())
+    }
+
+    @Bean
+    fun spotifyService(): ISpotifyService {
+        return SpotifyService(spotifyRepository())
     }
 }
--- src/test/kotlin/com/audio_insights_service/repositories/SpotifyRepositoryTest.kt
+++ src/test/kotlin/com/audio_insights_service/repositories/SpotifyRepositoryTest.kt
@@ -1,92 +1,96 @@
 package com.audio_insights_service.repositories
 
-import com.audio_insights_service.controllers.SpotifyController
 import com.audio_insights_service.entities.*
+import com.audio_insights_service.factories.createDummyTopArtistResponse
+import com.audio_insights_service.factories.createDummyTopTracksResponse
 import com.github.tomakehurst.wiremock.client.WireMock.*
 import com.github.tomakehurst.wiremock.junit5.WireMockTest
-import kotlinx.coroutines.runBlocking
-import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
-import org.springframework.web.reactive.function.client.WebClient
 import java.nio.file.Paths
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.springframework.web.reactive.function.client.WebClient
 
 @WireMockTest(httpPort = 8080)
 class SpotifyRepositoryTest {
-    private val wireMockServerUrl = "http://localhost:8080"
-/*
-@WireMockTest(httpPort = 8080)
+  private val wireMockServerUrl = "http://localhost:8080"
 
-    private val wireMockServerUrl = "http://localhost:8080"
+  @Test
+  fun `fetchProfile`() = runBlocking {
+    val dummyUserProfile =
+      UserProfile(
+        country = "US",
+        display_name = "John Doe",
+        email = "johndoe@example.com",
+        explicit_content = ExplicitContent(filter_enabled = true, filter_locked = false),
+        external_urls = ExternalURL(spotify = "https://open.spotify.com/user/johndoe"),
+        followers =
+          Followers(href = "https://api.spotify.com/v1/users/johndoe/followers", total = 1500),
+        href = "https://api.spotify.com/v1/users/johndoe",
+        id = "johndoe",
+        images =
+          arrayOf(Image(url = "https://i.scdn.co/image/abcd1234", height = 300, width = 300)),
+        product = "premium",
+        type = "user",
+        uri = "spotify:user:johndoe",
+      )
 
-    //Generate mock http server with WireMock and use real webclient
-    @Test
-    fun fetchProfile() = runBlocking {
-        val dummyUserProfile = UserProfile(
-            country = "US",
-            display_name = "John Doe",
-            email = "johndoe@example.com",
-            explicit_content = ExplicitContent(
-                filter_enabled = true,
-                filter_locked = false
-            ),
-            external_urls = ExternalURL(
-                spotify = "https://open.spotify.com/user/johndoe"
-            ),
-            followers = Followers(
-                href = "https://api.spotify.com/v1/users/johndoe/followers",
-                total = 1500
-            ),
-            href = "https://api.spotify.com/v1/users/johndoe",
-            id = "johndoe",
-            images = arrayOf(
-                Image(
-                    url = "https://i.scdn.co/image/abcd1234",
-                    height = 300,
-                    width = 300
-                )
-            ),
-            product = "premium",
-            type = "user",
-            uri = "spotify:user:johndoe"
+    val mockProfileJson =
+      Paths.get("./")
+        .toAbsolutePath()
+        .resolve(
+          "src/test/kotlin/com/audio_insights_service/repositories/resources/dummyUserProfile.json"
         )
+        .toFile()
+        .readText()
 
+    stubFor(get("/v1/me").willReturn(okJson(mockProfileJson)))
 
-        val mockProfileJson = Paths
-            .get("./")
-            .toAbsolutePath()
-            .resolve("src/test/kotlin/com/audio_insights_service/controllers/resources/dummyUserProfile.json")
-            .toFile()
-            .readText()
+    val webClient = WebClient.builder().baseUrl(wireMockServerUrl).build()
+    val repository = SpotifyRepository(webClient)
 
+    val result = repository.fetchProfile("Bearer mockToken")
 
-        stubFor(
-            get("/v1/me").willReturn(okJson(mockProfileJson)))
+    assertEquals(dummyUserProfile.toString(), result.toString())
+  }
 
-        val webClient = WebClient.builder()
-            .baseUrl(wireMockServerUrl)
-            .build()
-        // Instantiate the controller with the mocked WebClient
-        val controller = SpotifyController(webClient)
+  @Test fun `fetchTopTracks - need to implement`() = runBlocking {
+    val mockResponseJson =
+      Paths.get("./")
+        .toAbsolutePath()
+        .resolve(
+          "src/test/kotlin/com/audio_insights_service/repositories/resources/dummyTopTracksResponse.json"
+        )
+        .toFile()
+        .readText()
 
-        // Call the fetchProfile method
-        val result = controller.fetchProfile("Bearer mockToken")
+    stubFor(get("/v1/me/top/tracks").willReturn(okJson(mockResponseJson)))
 
-        // Assert that the returned UserProfile matches the mock
-        assertEquals(dummyUserProfile.toString(), result.toString())
-    }
-*/
-    @Test
-    fun `fetchProfile - need to implement`() = runBlocking {
-        assertEquals(1,1)
-    }
-    @Test
-    fun `fetchTopTracks - need to implement`() = runBlocking {
-        assertEquals(1, 1)
-    }
+    val webClient = WebClient.builder().baseUrl(wireMockServerUrl).build()
+    val repository = SpotifyRepository(webClient)
 
-    @Test
-    fun `fetchTopArtists - need to implement`() {
-        assertEquals(1,1)
-    }
+    val result = repository.fetchTopTracks("Bearer mockToken", "short_term")
+
+    assertEquals(createDummyTopTracksResponse().toString(), result.toString())
+  }
+
+  @Test
+  fun `fetchTopArtists`() = runBlocking {
+    val mockResponseJson =
+      Paths.get("./")
+        .toAbsolutePath()
+        .resolve(
+          "src/test/kotlin/com/audio_insights_service/repositories/resources/dummyTopArtistsResponse.json"
+        )
+        .toFile()
+        .readText()
+
+    stubFor(get("/v1/me/top/artists").willReturn(okJson(mockResponseJson)))
+
+    val webClient = WebClient.builder().baseUrl(wireMockServerUrl).build()
+    val repository = SpotifyRepository(webClient)
+
+    val result = repository.fetchTopArtists("Bearer mockToken", "short_term")
+
+    assertEquals(createDummyTopArtistResponse().toString(), result.toString())  }
 }
