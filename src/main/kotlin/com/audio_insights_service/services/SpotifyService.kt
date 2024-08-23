b510a00ed1ae4b5994bfff5b60724f5bdbfed30a
Matt Enterline <matthew.j.enterline@gmail.com>
2024-08-22 22:08:11 -0400

    added real implementation of fetchTopTracks and fetchTopArtists

--- src/main/kotlin/com/audio_insights_service/services/SpotifyService.kt
+++ src/main/kotlin/com/audio_insights_service/services/SpotifyService.kt
@@ -1,8 +1,6 @@
 package com.audio_insights_service.services
 
 import com.audio_insights_service.entities.*
-import com.audio_insights_service.factories.createDummyTopArtistResponse
-import com.audio_insights_service.factories.createDummyTopTracksResponse
 import com.audio_insights_service.repositories.ISpotifyRepository
 
 class SpotifyService(private val spotifyRepository: ISpotifyRepository) : ISpotifyService {
@@ -27,13 +25,13 @@ class SpotifyService(private val spotifyRepository: ISpotifyRepository) : ISpoti
   }
 
   override suspend fun fetchTopTracks(bearerToken: String, term: String): TopItemsResponse<Track> {
-    return createDummyTopTracksResponse()
+    return spotifyRepository.fetchTopTracks(bearerToken, term);
   }
 
   override suspend fun fetchTopArtists(
     bearerToken: String,
     term: String,
   ): TopItemsResponse<Artist> {
-    return createDummyTopArtistResponse()
+    return spotifyRepository.fetchTopArtists(bearerToken, term);
   }
 }
